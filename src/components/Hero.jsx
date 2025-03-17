3de0c5ccb632015f3c44001153ac246b2c740b6c
Badol-Hossain1 <badolhossain586@gmail.com>
2025-03-18 00:26:34 +0600

    feat: added leva for controls

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,18 @@ import { Suspense } from 'react'
 import CanvasLoader from './loader/CanvasLoader'
 import { Computer } from './computer/Computer'
 import { Environment, OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
 
 const Hero = () => {
+    const { rotationX, rotationY, rotationZ, scale } = useControls(
+        'Model Controls',
+        {
+            rotationX: { value: 0, min: -Math.PI, max: Math.PI, step: 0.1 },
+            rotationY: { value: 0, min: -Math.PI, max: Math.PI, step: 0.1 },
+            rotationZ: { value: 0, min: -Math.PI, max: Math.PI, step: 0.1 },
+            scale: { value: 1, min: 0.1, max: 5, step: 0.1 },
+        }
+    )
     return (
         <section className="min-h-screen w-full flex flex-col  relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -19,7 +29,11 @@ const Hero = () => {
                     <ambientLight intensity={0.5} />
                     <Suspense fallback={<CanvasLoader />}>
                         <OrbitControls />
-                        <Computer />
+                        <Computer rotation={[rotationX, rotationY, rotationZ]} scale={scale}  />
+                        <directionalLight
+                            position={[10, 10, 10]}
+                            intensity={0.5}
+                        />
                     </Suspense>
                 </Canvas>
             </div>
